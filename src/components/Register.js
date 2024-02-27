b0f2d786218b24109e5ffba317fc5506a275e056
Soumyajit Ghosh <113449899+someghosh0907@users.noreply.github.com>
2024-02-27 10:57:55 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Typography, TextField, Button } from "@mui/material";
-import toast from "react-hot-toast";
+//import toast from "react-hot-toast";
 import axios from "axios";
 const Register = () => {
   const navigate = useNavigate();
